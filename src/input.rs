5b42328310ca74ceb908bb895264e25c30ac6f0c
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-12-01 15:21:30 +0100

    Cache input

--- src/input.rs
+++ src/input.rs
@@ -2,15 +2,29 @@ use std::env;
 use reqwest::blocking::Client;
 
 
-pub fn get_input(day: i32, year: i32) -> Result<String, reqwest::Error> {
-    let url = format!("https://adventofcode.com/{}/day/{}/input", year, day);
 
+pub fn get_input(day: i32, year: i32) -> Result<String, Box<dyn std::error::Error>> {
     let cookie: String = env::var("AOC_SESSION").expect("You need to set the AOC_SESSION env variable with your cookie");
 
-    Client::new()
+    let url = format!("https://adventofcode.com/{}/day/{}/input", year, day);
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("aoc").unwrap();
+    let cache_path = xdg_dirs.place_cache_file(format!("{}/input/{}/{}", year, day, &cookie[..16])).unwrap();
+
+    if cache_path.exists() {
+        return Ok(std::fs::read_to_string(cache_path)?);
+    }
+
+
+
+    let result = Client::new()
         .get(url)
         .header("Cookie", format!("session={}", cookie))
         .send()?
-        .text()
+        .text()?;
+
+    std::fs::write(cache_path, &result).expect("Error caching input");
+
+    return Ok(result);
+
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod input;
 use crate::input::get_input;
 
 fn main() {
-    let input = get_input(1, 2022).expect("Failed getting input");
+    let input = get_input(1, 2023).expect("Failed getting input");
     println!("{}", input);
 }
